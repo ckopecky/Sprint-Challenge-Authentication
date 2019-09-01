ed34044cf4fc4fa61db3ca6dd6c83c7858d292b6
ckopecky <kopecky12112@gmail.com>
2019-08-31 22:38:16 -0700

    Add basic server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,41 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const session = require('express-session');
+const knexStore = require('connect-session-knex')(session);
 const cors = require('cors');
-
 const routes = require('./api/routes/routes');
+const bodyParser = require('body-parser');
 
-const server = express();
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true
 };
 
+const sessionOptions = {
+  name: 'cmvnk',
+  secret: 'cmvnklovescookies',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+    secure: false,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new knexStore({
+    knex: require('../database/dbConfig'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+}
+
+const server = express();
+
 server.use(bodyParser.json());
+server.use(express.json());
 server.use(cors(corsOptions));
+server.use(session(sessionOptions));
+server.use('/', routes);
 
-routes(server);
+module.exports = server;
 
-module.exports = {
-  server
-};
